42b9be7dc2c7d66577e8432abcea76d9d5f3836a
anton_miranouski <anton.miranouski@gmail.com>
2020-02-26 21:04:52 +0300

    fix

--- src/main/java/anton/miranouski/user_management/config/WebConfig.java
+++ src/main/java/anton/miranouski/user_management/config/WebConfig.java
@@ -5,13 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * The type Web config.
  */
 @Configuration
-@EnableWebMvc
 public class WebConfig {
 
     /**
